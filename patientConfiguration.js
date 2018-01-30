1b902ae5be9cd95b4fccc4fb563f8aff8100eb99
mkettelson <32883924+mkettelson@users.noreply.github.com>
2018-01-30 16:29:15 -0600

    trying to create video customization with external js

--- patientConfiguration.js
+++ patientConfiguration.js
@@ -1,3 +1,23 @@
 //Configure video play specific to patient on page 2
-let d = new Date();
-alert("Today's Date is" + d);
+
+
+function myFunction() {
+	
+	alert("in myFunction");
+	
+	if (document.getElementById("selection_1").checked = true){
+	alert("in if for video 1");
+	let variable = 1;
+	}
+	
+	// else if (document.getElementById("selection_2").checked = true){
+	// alert("in if for video 2");	
+	// }
+	
+	if (variable == 1){
+		alert("HELL YES");
+	}
+	else {
+		alert("almost");
+	}
+}
