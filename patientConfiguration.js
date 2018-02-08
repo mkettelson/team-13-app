b910285ddcfe95c67de60de0e964108244b42cb1
mkettelson <32883924+mkettelson@users.noreply.github.com>
2018-02-08 13:46:55 -0600

    YES! Progress with javascript functions to determine video parameters

--- patientConfiguration.js
+++ patientConfiguration.js
@@ -2,28 +2,33 @@
 
 
 function myFunction() {
+
 	
-	if (document.getElementById("selection_1").checked = true)
+	if (document.getElementById("selection_1").checked == true)
 	{
 		alert("in if for video 1");
 		let v = 1;
-	}
-	else
+	}	
+	if (document.getElementById("selection_3").checked == true)
 	{
-		let b = 1;
-		if (b == 1)
-		{
-			alert("HELL YES");
-		}
+		alert("in if for video 3");
+		let v = 3;
 	}
-
-
-	// // else if (document.getElementById("selection_2").checked = true){
-	// // alert("in if for video 2");	
-	// // }
-	
-
-	// else {
-		// alert("almost");
-	// }
+return v;	
 }
+
+//let v=1;
+//need another function to get v and in my script declare v and pass in into the function with parameters!
+function newFunc(v) {
+	//let v=1;
+	if(v==1)
+	{
+		alert("v is 1");
+	}
+	else if (v==3)
+	{
+		alert("v is 3")
+	}
+	
+}
+	
