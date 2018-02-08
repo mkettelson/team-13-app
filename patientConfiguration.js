d9b1ce67acb0d56b46e5392fb03456f021d5e9cb
mkettelson <32883924+mkettelson@users.noreply.github.com>
2018-02-08 17:02:38 -0600

    trying if statements to specify actions for a unique page to work with global variables

--- patientConfiguration.js
+++ patientConfiguration.js
@@ -1,9 +1,60 @@
 //Configure video play specific to patient on page 2
 
+//ATTEMPT 1
+// function selectVideo() {
+	// let v = 1;
+	// if (document.getElementById("selection_1").checked == true)
+	// {
+		// alert("in if for video 1");
+		// let v = 1;
+	// }	
+	// if (document.getElementById("selection_3").checked == true)
+	// {
+		// alert("in if for video 3");
+		// let v = 3;
+	// }
+	// getSelectedVideo(v);
+// }
+// function getSelectedVideo(v){
+	// videoSelected = v;
+	// alert("in getSelectedVideo"videoSelected);	
+// }
+// function playSelectedVideo() {
+	// let v=1;
+	// if(v==1)
+	// {
+		// alert("v is 1");
+	// }
+	// else if (v==3)
+	// {
+		// alert("v is 3")
+	// }
+//}
 
-function myFunction() {
 
-	
+//ATTEMPT 2	
+
+if(document.URL.indexOf("page2.html") >= 0){ 
+	v = selectVideo();
+	alert(v);
+	alert("in if for page 2");
+}
+
+if(document.URL.indexOf("page2a.html") >= 0){ 
+	if(v==1)
+	{
+		alert("v is 1");
+	}
+	else if (v==3)
+	{
+		alert("v is 3")
+	}
+}
+
+
+
+
+function selectVideo() {
 	if (document.getElementById("selection_1").checked == true)
 	{
 		alert("in if for video 1");
@@ -14,21 +65,7 @@ function myFunction() {
 		alert("in if for video 3");
 		let v = 3;
 	}
-return v;	
+	//getSelectedVideo(v);
+	return v;
 }
 
-//let v=1;
-//need another function to get v and in my script declare v and pass in into the function with parameters!
-function newFunc(v) {
-	//let v=1;
-	if(v==1)
-	{
-		alert("v is 1");
-	}
-	else if (v==3)
-	{
-		alert("v is 3")
-	}
-	
-}
-	
