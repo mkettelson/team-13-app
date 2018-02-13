3545dad0bd2035cef2719f492c85002425bb368b
KeiraChoi <jeeyu-choi@uiowa.edu>
2018-02-13 16:58:45 -0600

    yup

---
+++ page3.js
@@ -0,0 +1,7 @@
+function saveasPDF() {
+    String inputFile = "C:/Users/keirachoi/Documents/GitHub/team-13-app/page3.html";
+    String outputFile = "C:/Users/keirachoi/Desktop/TestPdf.pdf";
+    
+    System.out.println("Done!");
+
+}
