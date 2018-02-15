f80bd06e138db991a9c9cbea791e9c3c1ec982ff
mkettelson <32883924+mkettelson@users.noreply.github.com>
2018-02-15 17:48:27 -0600

    trying to get video to play from js

--- patientConfiguration.js
+++ patientConfiguration.js
@@ -1,81 +1,30 @@
-//Configure video play specific to patient on page 2
-
-//ATTEMPT 1
-// function selectVideo() {
-	// let v = 1;
-	// if (document.getElementById("selection_1").checked == true)
-	// {
-		// alert("in if for video 1");
-		// let v = 1;
-	// }	
-	// if (document.getElementById("selection_3").checked == true)
-	// {
-		// alert("in if for video 3");
-		// let v = 3;
-	// }
-	// getSelectedVideo(v);
-// }
-// function getSelectedVideo(v){
-	// videoSelected = v;
-	// alert("in getSelectedVideo"videoSelected);	
-// }
-// function playSelectedVideo() {
-	// let v=1;
-	// if(v==1)
-	// {
-		// alert("v is 1");
-	// }
-	// else if (v==3)
-	// {
-		// alert("v is 3")
-	// }
-//}
-
-
-//ATTEMPT 2	
-
-// if(document.URL.indexOf("page2.html") >= 0){ 
-	// v = selectVideo();
-	// alert(v);
-	// alert("in if for page 2");
-// }
-
-// if(document.URL.indexOf("page2a.html") >= 0){ 
-	// if(v==1)
-	// {
-		// alert("v is 1");
-	// }
-	// else if (v==3)
-	// {
-		// alert("v is 3")
-	// }
-// }
-
-
-//ATTEMPT 3: one HTML page with hidden components
-var t = 0;
+var v = 0;
 function selectVideo() {
 	if (document.getElementById("selection_1").checked == true)
 	{
 		//alert("in if for video 1");
-		var v = 1;
-        t = v;
-	}	
+        v = 1;
+        getVideo();
+	}
+	if (document.getElementById("selection_2").checked == true)
+	{
+		v = 2;
+        getVideo();
+	}
 	if (document.getElementById("selection_3").checked == true)
 	{
-		var v = 3;
-        t = v;
+		v = 3;
+        getVideo();
 	}
-    return t;
-}
-
-if (selectVideo() == 1)
-{
-    alert(selectVideo());
+	if (document.getElementById("selection_4").checked == true)
+	{
+		v = 4;
+        getVideo();
+	}
+    return v;
 }
 
 function getVideo(){
-	//videoSelection  = selectVideo();
-   // alert("");
+    alert("(get video) SELECTED VALUE : " + v);
+    return v;
 }
-
